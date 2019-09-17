497e18300ae33d6961b8511ab961130599c0d1ba
Alan Nunes da Silva <alann.625@gmail.com>
2019-09-17 16:39:19 -0300

    Create the project

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+namespace deadlockincsharp
+{
+    public class Program
+    {
+        static readonly object Tongue = new object();
+        static readonly object Throat = new object();
+        static void Speak()
+        {
+            lock (Tongue)
+            {
+                Console.WriteLine("Using the tongue.");
+                Thread.Sleep(1000);
+                lock (Throat)
+                {
+                    Console.WriteLine("Hello World!");
+                }
+            }
+        }
+        static void Eat()
+        {
+            lock (Throat)
+            {
+                Console.WriteLine("Using the throat.");
+                lock (Tongue)
+                {
+                    Console.WriteLine("Eating x-burguer.");
+                }
+            }
+        }
+        static void Main()
+        {
+            new Thread(new ThreadStart(Speak)).Start();
+            Thread.Sleep(500);
+            Eat();
+        }
+    }
+}
